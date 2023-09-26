aaa5d42e1f299ca2e123cdcf7b3b57f2c51086bc
Marina Dussenne <marina.pironti@gmail.com>
2023-09-26 15:41:54 +0200

    Creation about page and animation

--- src/Components/Collapse.js
+++ src/Components/Collapse.js
@@ -1,35 +1,34 @@
 import React, { useState } from 'react';
-import Arrow from '../assets/arrow.svg';
+import chevron from '../assets/chevron.png';
 
-const collapseData = [
-    { id: 1, title: "Fiabilité", text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." },
-    { id: 2, title: "Respect", text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." },
-    { id: 3, title: "Service", text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." },
-    { id: 4, title: "Sécurité", text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." }
-]
+function Collapse({ collapse, title, text }) {
+  const [openCollapse, setOpenCollapse] = useState(false);
+  const [isRotated, setIsRotated] = useState(false);
 
-function Collapse() {
-    const [openCollapse, setOpenCollapse] = useState(false)
+  const handleToggle = () => {
+    setOpenCollapse(!openCollapse);
+    setIsRotated(!isRotated);
+  };
 
-    return (
-        <div className="kasa-collapse">
-            {openCollapse ? (
-                <>
-                    {collapseData.map((item) => (
-                        <div key={item.id}>
-                            <button onClick={() => setOpenCollapse(false)}>
-                                <img src={Arrow} alt="Toggle" />
-                            </button>
-                            <p className='kasa-quality'>{item.title}</p>
-                            <p className='kasa-description'>{item.text}</p>
-                        </div>
-                    ))}
-                </>
-            ) : (
-                <button onClick={() => setOpenCollapse(true)}>{Arrow}</button>
-            )}
-        </div>
-    )
+  return (
+    <div className="kasa-collapse">
+      <p className='kasa-quality'>{title}</p>
+      {openCollapse ? (
+        <>
+          <div key={collapse}>
+            <button onClick={handleToggle} className={isRotated ? 'rotate180' : ''}>
+              <img src={chevron} alt='chevron' className='toogle' />
+            </button>
+            <p className='kasa-description'>{text}</p>
+          </div>
+        </>
+      ) : (
+        <button onClick={handleToggle}>
+          <img src={chevron} alt='chevron' className={`toogle ${isRotated ? 'rotate180' : ''}`} />
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Collapse;
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,11 +2,24 @@ import Banner from '../Components/Banner'
 import Collapse from '../Components/Collapse'
 import Montagne from '../assets/montagne.png'
 
+const collapseData = [
+  { id: 1, title: "Fiabilité", text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." },
+  { id: 2, title: "Respect", text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." },
+  { id: 3, title: "Service", text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." },
+  { id: 4, title: "Sécurité", text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." }
+]
+
 const About = () => {
 return(
   <div>
-    <Banner imageSource={Montagne}/>
-    <Collapse />
+    <Banner imageSource={Montagne} />
+    {collapseData.map(({collapse, title, text}) => (
+    <Collapse
+      key={collapse}
+      title={title}
+      text={text}
+    />
+    ))}
   </div>
   )
 }

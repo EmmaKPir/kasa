d810ca76fecb066102352e2ee7ddabd0e97bb821
Marina Dussenne <marina.pironti@gmail.com>
2023-10-04 16:21:55 +0200

    animation chevron ok

--- src/Components/Collapse.js
+++ src/Components/Collapse.js
@@ -3,29 +3,25 @@ import chevron from '../assets/chevron.png';
 
 function Collapse({ collapse, title, text }) {
   const [openCollapse, setOpenCollapse] = useState(false);
-  const [isRotated, setIsRotated] = useState(false);
 
   const handleToggle = () => {
     setOpenCollapse(!openCollapse);
-    setIsRotated(!isRotated);
   };
 
   return (
     <div className="kasa-collapse">
       <p className='kasa-quality'>{title}</p>
-      {openCollapse ? (
-        <>
-          <div key={collapse}>
-            <button onClick={handleToggle} className={isRotated ? 'rotate180' : ''}>
-              <img src={chevron} alt='chevron' className='toogle' />
-            </button>
-            <p className='kasa-description'>{text}</p>
-          </div>
-        </>
-      ) : (
-        <button onClick={handleToggle}>
-          <img src={chevron} alt='chevron' className={`toogle ${isRotated ? 'rotate180' : ''}`} />
-        </button>
+      <button onClick={handleToggle}>
+        <img
+          src={chevron}
+          alt='chevron'
+          className={`toogle ${openCollapse ? 'rotate180' : 'reverse'}`}
+        />
+      </button>
+      {openCollapse && (
+        <div key={collapse} className={`toogle kasa-description ${openCollapse ? 'openText' : 'closeText'}`}>
+          <p className='kasa-description'>{text}</p>
+        </div>
       )}
     </div>
   );
